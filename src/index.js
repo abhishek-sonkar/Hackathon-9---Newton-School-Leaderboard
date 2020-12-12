6c7edd62fc988d3c1ad2ee71797d2b4cc5451c03
abhisheksonkar20 <sonkar.abhishek45@gmail.com>
2020-12-12 17:54:17 +0530

    Updated Code

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get('/topRankings', (req, res) => {
+    const limit = isNaN(req.param('limit')) ? 20 : req.param('limit');
+    const offset = isNaN(req.param('offset')) ? 0 : req.param('offset');
+    const requiredPosts = data.slice(offset, limit);
+    res.send(requiredPosts);
+});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
